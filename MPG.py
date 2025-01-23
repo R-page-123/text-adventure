ed3f8dc1bbf5a5fe0f942cb22d1e08b460ed0e89
R-page-123 <ry.page@dameallans.co.uk>
2025-01-23 09:49:09 +0000

    Delete MPG.py

--- MPG.py
+++
@@ -1,7 +0,0 @@
-lastm = int(input("car mileage when last filled"))
-nowm = int(input("car milage now"))
-ltf = float(input("num of litres to fill tank"))
-miles = nowm - lastm
-g = ltf * 0.264172
-mpg = miles / g 
-print(mpg)
